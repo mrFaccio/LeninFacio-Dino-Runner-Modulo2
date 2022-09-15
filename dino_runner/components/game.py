7b1e159e6d87950522a428ce31908a480d1a9518
Lenin Facio <lenfacio@gmail.com>
2022-09-14 19:34:35 -0500

    Update game.py
    Add death count, score method and menu

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, FONT_STYLE
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
@@ -18,15 +18,27 @@ class Game:
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.running = False
+        self.score = 0
+        self.death_count = 0
+        
+    def execute(self):
+        self.running = True
+        while self.running:
+            if not self.playing:
+                self.show_menu()
+                pygame.time.delay(1000)
+        pygame.display.quit()
+        pygame.quit()
 
     def run(self):
         # Game loop: events - update - draw
+        self.obstacle_manager.reset_obstacles()
         self.playing = True
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
@@ -34,6 +46,7 @@ class Game:
                 self.playing = False
 
     def update(self):
+        self.update_score()
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
@@ -42,6 +55,7 @@ class Game:
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.draw_score()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         pygame.display.update()
@@ -55,3 +69,47 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+        
+    def draw_score(self):
+        font = pygame.font.Font(FONT_STYLE, 30)
+        text = font.render(f'Score: {self.score}', True, (0, 0, 0))
+        text_rec = text.get_rect()
+        text_rec.center = (1000, 50)
+        self.screen.blit(text, text_rec)
+
+    def update_score(self):
+        self.score += 1
+        
+        if self.score % 100 == 0 and self.game_speed < 700:
+            self.game_speed += 5
+        
+    
+    def handle_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                self.playing = False
+            elif event.type == pygame.KEYDOWN:
+                self.run()
+        
+    def show_menu(self):
+        print(self.death_count)
+        self.screen.fill((255, 255, 255))
+        half_screen_height = SCREEN_HEIGHT // 2
+        half_screen_width = SCREEN_WIDTH // 2
+        
+        if self.death_count == 0:
+            font = pygame.font.Font(FONT_STYLE, 30)
+            text = font.render('Press any key to start de game', True, (0, 0, 0))
+            text_rec = text.get_rect()
+            text_rec.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rec)
+        else:
+            pass
+            #Tarea
+            #Crear metodo para mostrar el texto
+            #Resetear score y game speed
+            #VISUALIZAR SCORE, DEATH_COUNT y GAME OVER
+        self.screen.blit(ICON, (half_screen_width -20, half_screen_height -140))
+        pygame.display.update()
+        self.handle_events_on_menu()
