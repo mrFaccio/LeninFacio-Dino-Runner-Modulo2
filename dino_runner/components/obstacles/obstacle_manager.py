17ae4d327d685812219bafba3f7cde50a95ec090
Lenin Facio <lenfacio@gmail.com>
2022-09-14 19:33:27 -0500

    Update obstacle_manager.py
    Add death count and reset_obstacles method

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,6 @@
 import pygame
 import random
 
-from cgitb import small
-
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 from dino_runner.components.obstacles.cactus import SmallCactus, LargeCactus
 from dino_runner.components.obstacles.bird import Bird
@@ -27,12 +25,15 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                print('Colisionó')
                 pygame.time.delay(1000)
+                game.death_count += 1
                 game.playing = False
                 break
             
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+            
+    def reset_obstacles(self):
+        self.obstacles = []
         
