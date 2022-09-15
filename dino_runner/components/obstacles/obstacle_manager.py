21d840fefedbae6cb89aeaebd207056bdbfa6192
Lenin Facio <lenfacio@gmail.com>
2022-09-14 20:06:12 -0500

    Update obstacle_manager
    Delete an unnecessary time.delay

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -25,7 +25,6 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(1000)
                 game.death_count += 1
                 game.playing = False
                 break
