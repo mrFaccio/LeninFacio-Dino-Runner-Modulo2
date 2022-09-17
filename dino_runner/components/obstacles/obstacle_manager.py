a0278c332a2b4d19963d312bc8cc1668ef923fe1
Lenin Facio <lenfacio@gmail.com>
2022-09-17 09:09:50 -0500

    Update obstacle_manager.py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -28,7 +28,7 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                if game.player.type != HAMMER_TYPE:
+                if game.player.type != HAMMER_TYPE and game.player.type != SHIELD_TYPE:
                     game.death_count += 1
                     game.playing = False
                 else:
