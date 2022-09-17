0be28fe359d6e9dc808e4f25113ac5615b033dc1
Lenin Facio <lenfacio@gmail.com>
2022-09-16 23:22:13 -0500

    Update obstacle_manager.py
    Add the conditional SHIELD_TYPE

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD, SHIELD_TYPE
 from dino_runner.components.obstacles.cactus import SmallCactus, LargeCactus
 from dino_runner.components.obstacles.bird import Bird
 
@@ -25,9 +25,12 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                game.death_count += 1
-                game.playing = False
-                break
+                if game.player.type != SHIELD_TYPE:
+                    game.death_count += 1
+                    game.playing = False
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
             
     def draw(self, screen):
         for obstacle in self.obstacles:
